e74a6920cc6be0bb057fef3404baa9b3d6c2cbd1
MonkeyDeLuffi <113219984+MonkeyDeLuffi@users.noreply.github.com>
2022-10-23 15:07:15 +0300

    initial commit

---
+++ SuperHeroAPI/Controllers/SuperheroController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperheroController : ControllerBase
+    {
+       private readonly DataContext dataContext;
+
+        public SuperheroController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHero>>> Get()
+        {
+            return Ok(await dataContext.SuperHeroes.ToListAsync());
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Get(int id)
+        {
+            var hero = await dataContext.SuperHeroes.FindAsync(id);
+            if (hero == null)
+                return BadRequest("Hero not found");
+            return Ok(hero);
+        }
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHero>>> AddHero(SuperHero hero)
+        {
+            dataContext.SuperHeroes.Add(hero);
+            return Ok(await dataContext.SuperHeroes.ToListAsync());
+        }
+        [HttpPut]
+        public async Task<ActionResult<List<SuperHero>>> UpdateHero(SuperHero request)
+        {
+            var dbHero = await dataContext.SuperHeroes.FindAsync(request.Id);
+            if (dbHero == null)
+                return BadRequest("Hero not found");
+
+            dbHero.Name = request.Name;
+            dbHero.FirstName = request.FirstName;
+            dbHero.LastName = request.LastName;
+            dbHero.Place = request.Place;
+
+            await dataContext.SaveChangesAsync();
+
+            return Ok(await dataContext.SuperHeroes.ToListAsync());
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHero>>> Delete(int id)
+        {
+            var dbHero = await dataContext.SuperHeroes.FindAsync(id);
+            if (dbHero == null)
+                return BadRequest("Hero not found");
+
+            dataContext.SuperHeroes.Remove(dbHero);
+
+            await dataContext.SaveChangesAsync();
+            return Ok(await dataContext.SuperHeroes.ToListAsync());
+        }
+
+
+    }
+}
